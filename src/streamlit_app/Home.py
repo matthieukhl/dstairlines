408129576c14578642f630d9105872d27f613a6b
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2024-09-04 16:25:54 +0200

    Changed app.py to Home.py

---
+++ src/streamlit_app/Home.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="DST Airlines",
+    page_icon=":airplane:"
+)
+st.title("DST Airlines :airplane:")
+st.subheader("Keep track of flights!")
